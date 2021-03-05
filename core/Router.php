4ec13f836cf53fa4588b90e23153757a361796a1
devusr <devusr@pop-os.localdomain>
2021-03-05 15:21:45 +0200

    finish router

--- core/Router.php
+++ core/Router.php
@@ -11,6 +11,8 @@ class Router
 
 
 
+
+
     public static function load($file)
     {
 
@@ -28,15 +30,14 @@ class Router
     }
 
 
-    public function direct($uri)
+    public function direct($uri, $requestType)
     {
-        if (array_key_exists($uri, $this->routes)) {
-            return $this->routes[$uri];
+        if (array_key_exists($uri, $this->routes[$requestType])) {
+            return $this->routes[$requestType][$uri];
         };
 
         throw new Exception('No route dfined for this url');
     }
-
 
     public function get($uri, $controller)
     {
